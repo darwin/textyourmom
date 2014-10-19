f5d7206db57ce7825fac29d9899b954428e3afb5
Antonin Hildebrand <antonin@hildebrand.cz>
2014-10-19 22:34:40 +0100

    generated from Xcode 6.1 template "Single View Application"

---
+++ CallMom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CallMom
+//
+//  Created by Antonin Hildebrand on 19/10/14.
+//  Copyright (c) 2014 tech beach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
