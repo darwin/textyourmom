97c9f4d8540d5bd11876a8749e1f75f60ffb8cbf
Antonin Hildebrand <antonin@hildebrand.cz>
2014-10-19 22:38:31 +0100

    removed comments in file headers

--- CallMom/AppDelegate.swift
+++ CallMom/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  CallMom
-//
-//  Created by Antonin Hildebrand on 19/10/14.
-//  Copyright (c) 2014 tech beach. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
--- CallMomTests/CallMomTests.swift
+++ CallMomTests/CallMomTests.swift
@@ -1,11 +1,3 @@
-//
-//  CallMomTests.swift
-//  CallMomTests
-//
-//  Created by Antonin Hildebrand on 19/10/14.
-//  Copyright (c) 2014 tech beach. All rights reserved.
-//
-
 import UIKit
 import XCTest
 
