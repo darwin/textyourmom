12d451f355bfcf81bda5a8175f3f82894d0aa278
Antonin Hildebrand <antonin@hildebrand.cz>
2014-10-20 18:17:26 +0100

    refactored Airport struct into a separate file

---
+++ CallMom/Airport.swift
@@ -0,0 +1,8 @@
+struct Airport {
+    var id: Int = 0
+    var name: String = ""
+    var city: String = ""
+    var country: String = ""
+    var latitude: Double = 0.0
+    var longitude: Double = 0.0
+}
--- CallMom/AirportsProvider.swift
+++ CallMom/AirportsProvider.swift
@@ -1,14 +1,5 @@
 import UIKit
 
-struct Airport {
-    var id: Int = 0
-    var name: String = ""
-    var city: String = ""
-    var country: String = ""
-    var latitude: Double = 0.0
-    var longitude: Double = 0.0
-}
-
 class AirportsProvider {
     var airports : [Airport]
     
