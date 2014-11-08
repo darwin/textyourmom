1ade4258fc35c7694bca7bb03a69f671d6ab798b
Antonin Hildebrand <antonin@hildebrand.cz>
2014-11-08 19:21:35 +0000

    changed outer perimeter distance to 15km

--- TextYourMom/Constants.swift
+++ TextYourMom/Constants.swift
@@ -5,6 +5,6 @@ let SINCE_IOS8 = isAtLeastIOS8()
 let PRIOR_IOS8 = !SINCE_IOS8
 
 let momCategoryString = "MomCategory"
-let outerAirportPerimeterDistance : CLLocationDistance = 5*1000 // TODO: read this from settings?
+let outerAirportPerimeterDistance : CLLocationDistance = 15*1000 // TODO: read this from settings?
 let innerAirportPerimeterDistance : CLLocationDistance = 3*1000 // TODO: read this from settings?
 let allowDeferredLocationUpdatesUntilTraveledDistance : CLLocationDistance = 500*1000 // 500km
